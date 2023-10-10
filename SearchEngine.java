8226954ec00583abe0e7b55365632a32c252f43b
Waterblokey <118576768+Waterblokey@users.noreply.github.com>
2023-10-10 09:51:16 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,5 @@
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);

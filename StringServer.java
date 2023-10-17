36b110af902a72d4b04de9b6e182f9c984a53c5f
Waterblokey <118576768+Waterblokey@users.noreply.github.com>
2023-10-17 16:04:33 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -17,7 +17,7 @@ class Handler implements URLHandler {
                 if (parameters[0].equals("s")) {
                     count += 1;
                     strCount = Integer.toString(count);
-                    str += "\n" + strCount + parameters[1];
+                    str += strCount + ". " + parameters[1] + "\n";
 //                    str += "\n %s. %d", strCount, parameters[1];
                     return String.format(str);
                 }
